b93f266f5ace1e2e9b281b89a097f5986829ac01
LizMarsh900 <em900@exeter.ac.uk>
2025-11-05 15:00:06 +0000

    hello world function added

--- course/intro/hello.py
+++ course/intro/hello.py
@@ -1,6 +1,5 @@
 def greet():
-    """Write a function that PRINTS 'Hello, World!'"""
-
+    print("Hello, World!")
 
 def add_two_numbers(a, b):
     """Write a function that adds the numbers a and b"""
