6ace8a321beda6a0bd47de65c667e02b50167421
LizMarsh900 <em900@exeter.ac.uk>
2025-11-11 11:55:13 +0000

    Completed exercise 1

--- course/exercises/exercises_1.py
+++ course/exercises/exercises_1.py
@@ -1,41 +1,39 @@
+# """Return the sum of all elements in the list 'numbers'."""
 def sum_list(numbers):
     return sum(numbers)
 
-
+# """Return the first element of the tuple 't'."""
 def first_of_tuple(t):
     return t[0]
 
+# """Return True if 'key' exists in dictionary 'd', else False."""
+def has_key(d, key): 
+    return key in d
 
-def has_key(d, key):
-    if key in d:
-      print('True')
-    Else:
-      print('False')
+# """Round the float 'f' to 2 decimal places."""
+def round_float(f): 
+    return round(f, 2)
 
+# """Return a new list that is the reverse of 'lst'."""
+def reverse_list(lst): 
+    return lst[::-1]
 
-def round_float(f):
-    round(f, 2)
-
-
-def reverse_list(lst):
-    return lst.reverse()
-
-
+# """For a list of items 'lst', count how many times element 'item' occurs."""
 def count_occurrences(lst, item):
-    list.count('item')
+    return lst.count(item) 
 
-
+# """Convert a list of (key, value) tuples 'pairs' into a dictionary."""
 def tuples_to_dict(pairs):
-    dict(pairs)
+    return dict(pairs) 
 
-
+# """Return the number of characters in string 's'."""
 def string_length(s):
     return len(s)
 
-
+# """Return a list of unique elements from 'lst'."""
 def unique_elements(lst):
     return set(lst)
 
-
-def swap_dict(d):
-    return 0
+# """Return a new dictionary with keys and values of 'd' swapped."""
+def swap_dict(d): #
+    return {v: k for k, v in d.items()}
