b8d01c54e7940b9b629b972873750661151758a6
LizMarsh900 <em900@exeter.ac.uk>
2025-11-12 15:03:38 +0000

    Altered exercises 1 file

--- course/exercises/exercises_1.py
+++ course/exercises/exercises_1.py
@@ -1,39 +1,48 @@
-# """Return the sum of all elements in the list 'numbers'."""
 def sum_list(numbers):
+    """Return the sum of all elements in the list 'numbers'."""
     return sum(numbers)
 
-# """Return the first element of the tuple 't'."""
+
 def first_of_tuple(t):
+    """Return the first element of the tuple 't'."""
     return t[0]
 
-# """Return True if 'key' exists in dictionary 'd', else False."""
-def has_key(d, key): 
+
+def has_key(d, key):
+    """Return True if 'key' exists in dictionary 'd', else False."""
     return key in d
 
-# """Round the float 'f' to 2 decimal places."""
+
 def round_float(f): 
+    """Round the float 'f' to 2 decimal places."""
     return round(f, 2)
 
-# """Return a new list that is the reverse of 'lst'."""
+
 def reverse_list(lst): 
+    """Return a new list that is the reverse of 'lst'."""
     return lst[::-1]
 
-# """For a list of items 'lst', count how many times element 'item' occurs."""
+ 
 def count_occurrences(lst, item):
+    """For a list of items 'lst', count how many times element 'item' occurs."""
     return lst.count(item) 
 
-# """Convert a list of (key, value) tuples 'pairs' into a dictionary."""
+
 def tuples_to_dict(pairs):
+    """Convert a list of (key, value) tuples 'pairs' into a dictionary."""
     return dict(pairs) 
 
-# """Return the number of characters in string 's'."""
+
 def string_length(s):
+    """Return the number of characters in string 's'."""
     return len(s)
 
-# """Return a list of unique elements from 'lst'."""
+
 def unique_elements(lst):
+    """Return a list of unique elements from 'lst'."""
     return set(lst)
 
-# """Return a new dictionary with keys and values of 'd' swapped."""
-def swap_dict(d): #
+
+def swap_dict(d): 
+    """Return a new dictionary with keys and values of 'd' swapped."""
     return {v: k for k, v in d.items()}
